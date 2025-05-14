9738bb852e4ca756815367a5d613778d7f8fda1f
heisdinesh <dinesh35382@gmail.com>
2025-05-15 05:08:04 +0530

    Enhancement: Made a sticky header in the dashboard page, now attribute and taxon set can be modified easily.

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -51,79 +51,84 @@ const Dashboard = () => {
   console.log("🚀 ~ Dashboard ~ countsByTaxonData:", countsByTaxonData);
 
   return (
-    <div className={styles.page}>
-      <h1>KinFin Analysis</h1>
-      <AttributeSelector />
-      <RunSummary />
-      <div className={styles.chartsContainer}>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <p className={styles.title}>Attribute Summary</p>
-          </div>
-          <div className={styles.chartContainer}>
-            <AttributeSummary />
-          </div>
-        </div>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <p className={styles.title}>Cluster Summary</p>
-          </div>
-          <div className={styles.chartContainer}>
-            <ClusterSummary />
-          </div>
-        </div>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <p className={styles.title}>Cluster Metrics</p>
-          </div>
-          <div className={styles.chartContainer}>
-            <ClusterMetrics />
-          </div>
-        </div>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <p className={styles.title}>Cluster Size Distribution</p>
-          </div>
-          <div className={styles.chartContainer}>
-            <ClusterSizeDistribution />
-          </div>
-        </div>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <p className={styles.title}>All Rarefaction Curve</p>
-          </div>
-          <div className={styles.chartContainer}>
-            <AllRarefactionCurve />
-          </div>
-        </div>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <p className={styles.title}>Cluster Distribution Per Taxon</p>
-          </div>
-          <div className={styles.chartContainer}>
-            <ClusterAndProteinDistributionPerTaxonSet />
-          </div>
-        </div>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <p className={styles.title}>Cluster Absence Across Taxon Sets</p>
-          </div>
-          <div className={styles.chartContainer}>
-            <ClusterAbsenceAcrossTaxonSets />
-          </div>
-        </div>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <p className={styles.title}>Taxon Count per Taxon Set</p>
-          </div>
-          <div className={styles.chartContainer}>
-            <TaxonCountPerTaxonSet />
-          </div>
-        </div>
+    <>
+      {" "}
+      <div className={styles.pageHeader}>
+        <h1 className={styles.pageTitle}>KinFin Analysis</h1>
+        <AttributeSelector />
       </div>
+      <div className={styles.page}>
+        <RunSummary />
+        <div className={styles.chartsContainer}>
+          <div className={styles.container}>
+            <div className={styles.header}>
+              <p className={styles.title}>Attribute Summary</p>
+            </div>
+            <div className={styles.chartContainer}>
+              <AttributeSummary />
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.header}>
+              <p className={styles.title}>Cluster Summary</p>
+            </div>
+            <div className={styles.chartContainer}>
+              <ClusterSummary />
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.header}>
+              <p className={styles.title}>Cluster Metrics</p>
+            </div>
+            <div className={styles.chartContainer}>
+              <ClusterMetrics />
+            </div>
+          </div>
+          {/* <div className={styles.container}>
+            <div className={styles.header}>
+              <p className={styles.title}>Cluster Size Distribution</p>
+            </div>
+            <div className={styles.chartContainer}>
+              <ClusterSizeDistribution />
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.header}>
+              <p className={styles.title}>All Rarefaction Curve</p>
+            </div>
+            <div className={styles.chartContainer}>
+              <AllRarefactionCurve />
+            </div>
+          </div> */}
+          <div className={styles.container}>
+            <div className={styles.header}>
+              <p className={styles.title}>Cluster Distribution Per Taxon</p>
+            </div>
+            <div className={styles.chartContainer}>
+              <ClusterAndProteinDistributionPerTaxonSet />
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.header}>
+              <p className={styles.title}>Cluster Absence Across Taxon Sets</p>
+            </div>
+            <div className={styles.chartContainer}>
+              <ClusterAbsenceAcrossTaxonSets />
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.header}>
+              <p className={styles.title}>Taxon Count per Taxon Set</p>
+            </div>
+            <div className={styles.chartContainer}>
+              <TaxonCountPerTaxonSet />
+            </div>
+          </div>
+        </div>
 
-      {/* <CountsByTaxonChart data={countsByTaxonData} /> */}
-    </div>
+        {/* <CountsByTaxonChart data={countsByTaxonData} /> */}
+      </div>
+    </>
   );
 };
 
