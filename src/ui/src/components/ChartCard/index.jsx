a57141d0aea20b20bc81cb0fde1a5e66e6b57e71
heisdinesh <dinesh35382@gmail.com>
2025-08-07 17:37:46 +0530

    refactor: Dashbord page tables and charts are rendered through ChartCard
    - Cleaned redudant scss.

--- src/ui/src/components/ChartCard/index.jsx
+++ src/ui/src/components/ChartCard/index.jsx
@@ -3,9 +3,19 @@ import styles from "./ChartCard.module.scss";
 import { MdOutlineFileDownload } from "react-icons/md";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
-const ChartCard = ({ title, isDownloading, onDownload, onOpen, children }) => {
+const ChartCard = ({
+  widthPercent = 100,
+  title,
+  isDownloading,
+  onDownload,
+  onOpen,
+  children,
+}) => {
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={{ flex: `1 1 ${widthPercent}%`, maxWidth: `${widthPercent}%` }}
+    >
       <div className={styles.header}>
         {onOpen && (
           <button className={styles.enlargeButton} onClick={onOpen}>
